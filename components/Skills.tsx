7a1c2c346b7f2b116eb57454b5739ccadfb1baa9
Ishita Agarwal <118765693+ishita-139@users.noreply.github.com>
2026-01-04 23:37:21 +0530

    Update Skills.tsx

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Code, Server, Database, PenTool as Tool } from 'lucide-react';
+import { Code,Cpu, Server, Database, PenTool as Tool } from 'lucide-react';
 import { Card } from './ui/card';
 
 interface SkillProps {
