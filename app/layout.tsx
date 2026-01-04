fb81e10a6aadc0c33b652992eeff7a60c315b490
Ishita Agarwal <118765693+ishita-139@users.noreply.github.com>
2026-01-04 23:56:01 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,19 +5,19 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Rajat Kumar Thakur | Portfolio',
-  keywords: ['Rajat Kumar Thakur', 'Full Stack Developer', 'Next.js', 'Node.js', 'MongoDB', 'JavaScript', 'Portfolio'],
-  authors: [{ name: 'Rajat Kumar Thakur' }],
-  creator: 'Rajat Kumar Thakur',
-  publisher: 'Rajat Kumar Thakur',
+  title: 'Ishita Agarwal | Portfolio',
+  keywords: ['Ishita Agarwal', 'Full Stack Developer', 'Next.js', 'Node.js', 'MongoDB', 'JavaScript', 'Portfolio'],
+  authors: [{ name: 'Ishita Agarwal' }],
+  creator: 'Ishita Agarwal',
+  publisher: 'Ishita Agarwal',
   viewport: 'width=device-width, initial-scale=1',
   robots: 'index, follow',
   openGraph: {
     type: 'website',
     locale: 'en_US',
     url: 'https://rajatkumarthakur.vercel.app',
-    title: 'Rajat Kumar Thakur | Portfolio',
-    siteName: 'Rajat Kumar Thakur Portfolio',
+    title: 'Ishita Agarwal | Portfolio',
+    siteName: 'Ishita Agarwal Portfolio',
   }
 };
 
